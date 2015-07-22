c8a4d7a974eb7fb1796eb0ff1d80f107c95d7f27
chuchu <cobiekwe1@gmail.com>
2015-07-22 13:54:12 -0400

    Strings assingment completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,36 +11,53 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+
+   //    NSMutableString *favoriteCheese = [NSMutableString stringWithString:@"My favorite cheese is "];
+   //  [favoriteCheese appendString:cheeseName];
+     NSString *favoriteCheese = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    
+    return favoriteCheese;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
-        /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
+        NSString *nameWithoutCheese = cheeseName;
+        
+        //[nameWithoutCheese stringByReplacingOccurrencesOfString:@" " withString:@""];
+        NSRange noCheeseRange = [nameWithoutCheese rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+        NSString *newCheeseName = [nameWithoutCheese stringByReplacingCharactersInRange:noCheeseRange withString:@""];
+    
+        return newCheeseName;
+    
     } else {
-        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+    
+        return cheeseName;
     }
 
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
-
-    return nil;
+     // return newCheeseName;
+    
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        NSString *newCheeseCount = [NSString stringWithFormat:@"%ld cheese", (long)cheeseCount];
+    
+        return newCheeseCount;
+        
     } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+            NSString *newCheeseCount = [NSString stringWithFormat:@"%ld cheeses", (long)cheeseCount];
+            
+            return newCheeseCount;
     }
     
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
     
-    return nil;
+    //return nil;
 }
 
 @end
