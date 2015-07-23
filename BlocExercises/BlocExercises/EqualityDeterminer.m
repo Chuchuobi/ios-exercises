f9789613aa5735446357dac28c5dc579c2b09517
chuchu <cobiekwe1@gmail.com>
2015-07-22 20:38:00 -0400

    Equality assingment completed

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,30 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
-    return (string1 == string2);
+    
+    BOOL areTheyEqual = [string1 isEqualToString: string2];
+    
+    NSString *result = areTheyEqual ? @"Yes" : @"No";
+    
+    
+    return result;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+    
+    BOOL areTheyEqual = [number1 isEqualToNumber: number2];
+    
+    NSString *result = areTheyEqual ? @"Yes" : @"No";
+    
+    return result;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    
+    
+    BOOL isGreater = integer1 > integer2 ? @"Yes" : @"No";
+    
+    return isGreater;
 }
 
 @end
